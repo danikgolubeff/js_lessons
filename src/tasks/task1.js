5ce59174d3842b185f0fc96310cfce05bc8e28bd
danikgolubeff <dan.golubev@gmail.com>
2020-10-13 23:44:25 +0300

    connectjs

---
+++ src/tasks/task1.js
@@ -0,0 +1,20 @@
+alert('Доброе утро');
+
+const sayHello = (name) => {
+    if (typeof name === "string") {
+
+        return `Hello, ${name}`;
+    }
+}
+const name = "Oleg"
+
+console.log(sayHello(name));
+
+sayHellow = function (name) {
+    if (typeof name === "string") {
+
+        return `Hello, ${name}`;
+    }
+}
+alert(sayHellow('Victor'));
+alert('Доброе утро');
