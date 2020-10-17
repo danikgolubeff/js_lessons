5e76b588573f7cbe95777ef3447bc67fe363ebe9
IT Shatle <itshatle@iMac-Lomray.local>
2020-10-17 16:29:18 +0300

    “classlessmap”

---
+++ src/tasks/classtasks.js
@@ -0,0 +1,11 @@
+const numberArray = [1, 2, 3, 4];
+if (!Array.isArray(numberArray)) return;
+const userNumber = (array) => {
+    {
+        return array.map((numberArray)) => {
+    return `User ${number.toString()}`;
+}
+
+
+
+console.log(userNumber());
